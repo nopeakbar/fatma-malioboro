159e1f25d18b22bf721b5cf31540370a9bc0904b
nopeakbar <nopeakbar@gmail.com>
2025-12-24 01:34:20 +0700

    tes

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},  // Pakai 'tailwindcss', BUKAN '@tailwindcss/postcss'
-    autoprefixer: {}, // WAJIB ADA
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
 
